4c9acf7a9225fea3f5776566c9b3526d0c7cb84b
Antonio Jose <antoniojose@MacBook-Pro-de-Antonio.local>
2020-06-28 19:32:26 -0500

    Tabla cantidad de muestras médicas, receptores y reportadores

--- TVSS_2019_2.R
+++ TVSS_2019_2.R
@@ -1,6 +1,6 @@
 #Importando cada uno de los archivos desde el origen
 
-#Se deben importar cada uno de los datos exportados desde PISIS
+#Se deben importar cada uno de los datos exportados desde PISIS (Pendiente)
 
 #Luego de importar cada uno de los archivos se consolida en uno solo
 TVSS_2019_2 <- rbind(ConsultaData1, ConsultaData2, ConsultaData3, ConsultaData4, ConsultaData5, ConsultaData6, ConsultaData7,ConsultaData8)
@@ -26,20 +26,26 @@ TVSS_2019_2$MontoEjecutadoTV <- as.numeric(TVSS_2019_2$MontoEjecutadoTV)
 #Gráfico monto ejecutado por fecha
 plot(TVSS_2019_2$FechaTV, TVSS_2019_2$MontoEjecutadoTV)
 
-
 install.packages("dplyr")
 library("dplyr")
 
-#Tabla cantidad de transferencias, receptores y valor del monto por entidad
+#Tabla cantidad de transferencias, RECEPTORES y valor del monto por entidad
 Entidades <- TVSS_2019_2 %>% group_by(FuenteNombreEntidadKReporta) %>% 
   summarise(Transferencias = n(), Receptores = n_distinct(NumIDReceptor), 
             Monto = sum(MontoEjecutadoTV, na.rm = TRUE))
 
-#Tabla cantidad de transferencias, reporotadores y monto por receptor
+#Tabla cantidad de transferencias, REPORTADORES y monto por receptor
 Receptores <- TVSS_2019_2 %>% group_by(NumIDReceptor) %>% 
   summarise(Transferencias = n(), Reportador = min(FuenteNroIdReportador),
             CantidadReportadores = n_distinct(FuenteNroIdReportador),
           Monto = sum(MontoEjecutadoTV, na.rm = TRUE))
+
+#Tabla cantidad de muestras médicas, receptores y reportadores
+MuestrasMedicas <- filter(TVSS_2019_2, TipoTransferenciaValor == 10)
+MuestrasMedicas <- MuestrasMedicas %>% group_by(NroIdMuestraMedica) %>% 
+  summarise(Transferencias = n(), Reportador = min(FuenteNroIdReportador),
+            CantidadReportadores = n_distinct(FuenteNroIdReportador),
+            Cantidad = sum(NumMuestrasMedicasEntregadas, na.rm = TRUE))
 
 #Casos específicos
 filter(TVSS_2019_2, NombreReceptor == "CUENTA DE ALTO COSTO")
@@ -66,10 +72,6 @@ library("modeest")
 frecuenciasTipoTransferencias <- table(TVSS_2019_2$TipoTransferenciaValor)
 prop.table(frecuenciasTipoTransferencias)
 
-plot(x = PersonasNaturales$TipoTransferenciaValor,  y = PersonasNaturales$NumIDReceptor)
-
-save(TVSS_2019_2, file="TVSS_2019_2")
-
 Receptores_menores4digitos<- TVSS_2019_2 %>% group_by(NumIDReceptor) %>% 
   summarise(Transferencias = n(), Reportador = min(FuenteNroIdReportador), 
             Monto = sum(MontoEjecutadoTV, na.rm = TRUE))
@@ -78,3 +80,5 @@ Receptores_Duda <- filter(Receptores, Receptores$CantidadReportadores == 1,
                           Receptores$Monto > 0)
 
 write.csv(Entidades, file="Entidades.csv")
+
+#Pendiente ver relación entre muestras médicas, cantidad de monto de TV y prescripciones
